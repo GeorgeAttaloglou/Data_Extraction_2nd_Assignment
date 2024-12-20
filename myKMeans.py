9da61b30c7072db97d5ad22548a28bd4b0a8cbc2
GeorgeAttaloglou <georgeeattaloglou@gmail.com>
2024-12-20 09:40:16 +0200

    Add working centroid initializer func

--- myKMeans.py
+++ myKMeans.py
@@ -1,6 +1,17 @@
 import numpy as np
 import random
 
+def centroids(dimensions:int, k:int) -> list:
+    list_of_centroids = []
+
+    for _ in range(k):
+        centroid = []
+        for _ in range(dimensions):
+            centroid.append(random.randrange(0,20))
+        list_of_centroids.append(centroid)
+    
+    return list_of_centroids
+
 def myKmeans(data:list, k:int):
     """
     The data list is rows: 1 for each object columns: ints of data for it like coordinates; data[0]=(1,2), data[1]=(3,2)
@@ -10,16 +21,13 @@ def myKmeans(data:list, k:int):
     a list of the cluster values for each point of data
 
     """
-    centroid_coordinates_init = []
     dimensions = len(data[0])
-
-    for _ in range(1,dimensions):
-        centroid_coordinates_init.append(random.randrange(0,20))
-
-    return centroid_coordinates_init
+    centroids = centroids(dimensions,k)
+    return centroids
 
 def main():
-    if __name__ == "__main__":
-        data = [(1,2),(3,2),(1,3),(2,3),(8,9),(9,8),(8,8),(9,9)]
-        k = 2
-        print(myKmeans(data, k))
+    test = centroids(2,5)
+    print(test)
+
+if __name__ == "__main__":
+    main()
